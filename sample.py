764d1caab1d6f2f2806f06efb63e626ac45c10a9
CyberWarrior404 <mdmor@icloud.com>
2026-02-07 08:44:52 -0600

    Add files via upload

--- sample.py
+++ sample.py
@@ -1,14 +1,24 @@
 text = "I Love Coding"
-print("original string: ", text)
-print("length of string: ",len(text))
-print("frist charecter: ", text[0])
-print("last charecter: ", text[-1])
-print("\nlooping through charecters: ")
+
+print("Original string:", text)
+print("Length of string:", len(text))
+print("First character:", text[0])
+print("Last character:", text[-1])
+
+print("\nLooping through characters:")
 for ch in text:
-    print(ch,end=" ")
+    print(ch, end=" ")
 print()
 
-print("\nusing in and not in operators: ")
-print("'Love' in text: ", "Love" in text)
-print("'JAVA' not in text: ", "JAVA" not in text)
+print("\nin and not in operators:")
+print("'Love' in text:", "Love" in text)
+print("'JAVA' not in text:", "JAVA" not in text)
+
+print("\nstring methods:")
+print("Lowercase:", text.lower())
+print("Uppercase:", text.upper())
+print("Capitalize:", text.capitalize())
+print("Title case:", text.title())
+print("Swapcase:", text.swapcase())
+
 
